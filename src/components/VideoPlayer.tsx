6eef73656a1c0ae0dbab5382c65884f06f8d2fa2
Jay <jayay98@gmail.com>
2022-01-15 23:58:25 +0100

    First commit

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -19,31 +19,27 @@ const initialOptions: videojs.PlayerOptions = {
 }
 
 const VideoPlayer: React.FC<IVideoPlayerProps> = ({ options }) => {
+  // References For Video Player
   const videoNodeRef = useRef<HTMLVideoElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
   const playerRef = useRef<videojs.Player>()
+
   const sourceRef = useRef<MediaElementAudioSourceNode>()
   const audioCtxRef = useRef<AudioContext>(new AudioContext())
-  // const audioNodeRef = useRef<HTMLAudioElement>(null);
+  const canvasCtxRef = useRef<CanvasRenderingContext2D>()
   const [audioUrls, setAudioUrls] = useState<string[]>([])
   const addAudioUrl = (url: string) => {
     setAudioUrls([...audioUrls, url])
   }
 
-  // const md = new MediaRecorder()
+  const analyser = audioCtxRef.current.createAnalyser()
+  analyser.fftSize = 256
+  const bufferLength = analyser.frequencyBinCount
+  const dataArray = new Uint8Array(bufferLength)
+  analyser.getByteTimeDomainData(dataArray)
 
-  let CurY: number
-  const HEIGHT = window.innerHeight
-  const panNode = audioCtxRef.current.createStereoPanner()
   const recordingStream = audioCtxRef.current.createMediaStreamDestination()
   const recorder = new MediaRecorder(recordingStream.stream)
-
-  function updatePage (e: MouseEvent) {
-    CurY = (window.Event) ? e.pageY : e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop)
-
-    // gainNode.gain.value = CurY/HEIGHT;
-    panNode.pan.value = (CurY / (HEIGHT / 2)) - 1
-  }
-  document.onmousemove = updatePage
 
   function startrecording () {
     sourceRef.current?.connect(recordingStream)
@@ -52,27 +48,33 @@ const VideoPlayer: React.FC<IVideoPlayerProps> = ({ options }) => {
 
   function stoprecording () {
     recorder.addEventListener('dataavailable', function (e) {
-      // audioNodeRef.current!.src = URL.createObjectURL(e.data);
       const url = URL.createObjectURL(e.data)
       addAudioUrl(url)
-      console.log(audioUrls)
     })
     recorder.stop()
   }
 
+  // Update sourceRef
   useEffect(
     () => {
       if (videoNodeRef.current) {
         if (sourceRef.current == null) {
           sourceRef.current = audioCtxRef.current.createMediaElementSource(videoNodeRef.current as HTMLMediaElement)
+          console.log('MediaElementSourceNode created')
         }
         sourceRef.current.connect(audioCtxRef.current.destination)
-        // panNode.connect(audioCtxRef.current.destination);
+        sourceRef.current.connect(analyser)
+        console.log('MediaElementSourceNode connected to destination')
       }
+
+      if (canvasRef.current) {
+        canvasCtxRef.current = canvasRef.current.getContext('2d')!
+        draw(canvasCtxRef.current)
+      }
+
       return () => {
         console.log('disconnect')
         sourceRef.current?.disconnect()
-        // panNode.disconnect();
       }
     }
   )
@@ -92,15 +94,46 @@ const VideoPlayer: React.FC<IVideoPlayerProps> = ({ options }) => {
         player.autoplay(true)
         player.src(options.sources!)
       }
+
+      if (canvasRef.current) {
+        draw(canvasRef.current.getContext('2d')!)
+      }
     }, [options]
   )
+
+  function draw (canvasCtx: CanvasRenderingContext2D) {
+    const WIDTH = canvasRef.current!.width
+    const HEIGHT = canvasRef.current!.height
+    requestAnimationFrame(() => draw(canvasCtx))
+
+    analyser.getByteFrequencyData(dataArray)
+
+    canvasCtx.fillStyle = 'rgb(0, 0, 0)'
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
+
+    const barWidth = (WIDTH / bufferLength) * 2.5
+    let barHeight: number
+    let x = 0
+
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i] / 2
+
+      canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)'
+      canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight)
+
+      x += barWidth + 1
+    }
+  }
 
   return (
         <>
             <video ref={videoNodeRef} className='video-js vjs-big-play-centered' id='playerElement' />
-            {/* <audio controls ref={audioNodeRef} /> */}
+            <canvas ref={canvasRef} style={{ width: 500, height: 500 }}/>
             {audioUrls.map((url, i) => (
+              <>
                 <audio controls src={url} key={`audio-${i}`}/>
+                <a href={url} download={`${i}.ogg`}>Download</a>
+              </>
             ))}
             <Button onClick={startrecording}>Start</Button>
             <Button onClick={stoprecording}>End</Button>
